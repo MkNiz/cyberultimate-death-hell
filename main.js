66e04bec92decf4745888e0d986b339758641538
Michael Nizzari <michael.nizzari@gmail.com>
2017-09-09 14:23:11 -0400

    Basic player variables

---
+++ main.js
@@ -0,0 +1,9 @@
+/*
+ | Cyberultimate Death Hell
+ | v0.0.0.1
+ | by Michael Nizzari (a.k.a. CYBEReris)
+*/
+
+var playerName = "Biokeith Killgood";
+var playerHealth = 120;
+var playerLocation = "West Burnsfire";
